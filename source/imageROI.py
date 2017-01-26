b2031f58b978048f45d1691799d2213b89e65c88
Marcelo Cabral Ghilardi <kabrau@gmail.com>
2017-01-25 23:30:08 -0200

    First Sample

--- source/imageROI.py
+++ source/imageROI.py
@@ -1,2 +1,78 @@
+# Capturing image ROI with Python and OpenCV
+# 25/01/2017 - Marcelo Cabral 
+# I used this base example: http://www.pyimagesearch.com/2015/03/09/capturing-mouse-click-events-with-python-and-opencv/
 
-#imageROI.py
+# import the necessary packages
+import argparse
+import cv2
+
+# initialize the list of reference points and boolean indicating
+# whether cropping is being performed or not
+refPt = []
+cropping = False
+ 
+def click_and_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global refPt, cropping
+ 
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+        cropping = True
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if cropping:
+            image2 = clone.copy()
+            cv2.rectangle(image2, refPt[0], (x, y), (0, 255, 0), 2)
+            cv2.imshow("image", image2)
+
+    # check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates and indicate that
+        # the cropping operation is finished
+        refPt.append((x, y))
+        cropping = False
+ 
+        # draw a rectangle around the region of interest
+        #image = clone.copy()
+        cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+        cv2.imshow("image", image)
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+# load the image, clone it, and setup the mouse callback function
+image = cv2.imread(args["image"])
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
+
+# keep looping until the 'q' key is pressed
+while True:
+    # display the image and wait for a keypress
+    cv2.imshow("image", image)
+    key = cv2.waitKey(1) & 0xFF
+ 
+    # if the 'r' key is pressed, reset the cropping region
+    if key == ord("r"):
+        image = clone.copy()
+ 
+    # if the 'c' key is pressed, break from the loop
+    elif key == ord("c"):
+        break
+
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 2:
+    print refPt[0][1],refPt[1][1], refPt[0][0],refPt[1][0] 
+    roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+    cv2.imshow("ROI", roi)
+    cv2.waitKey(0)
+ 
+# close all open windows
+cv2.destroyAllWindows()
